3264f9c1c5b8bc81d7b63810b45df21564598827
nsz23 <161598619+nsz23@users.noreply.github.com>
2024-04-03 11:57:55 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+<div class="container-fluid">
+    <footer class="row bg-dark text-light p-3">
+        <div class="col-md-4">
+            <h5>Kontaktujte nás:</h5>
+            <p>Email: info@example.com</p>
+            <p>Telefón: +123456789</p>
+        </div>
+
+        <div class="col-md-4">
+            <h5>Presmerovanie:</h5>
+            <ul class="list-unstyled">
+                <li><a href="index.php" style="color: white;">Domov</a></li>
+                <li><a href="o_nas.php" style="color: white;">O nás</a></li>
+            </ul>
+        </div>
+        
+        <div class="col-md-4">
+            <br><br><br>
+            <h6> @<?php echo date("Y"); ?> Nela Szárazová</h6>
+        </div>
+    </footer>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
